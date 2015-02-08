46c927e8b41322fa6831e39bacce69371e86d856
deniznida <denizunat@hotmail.com>
2015-02-08 14:55:49 -0500

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+
+  if num == 1
+    false
+  end
+  if num == 2
+    true
+  end
+  is_prime = true
+  for i in 2...num do
+    #puts "#{i}"
+    if num % i == 0
+      is_prime = false
+      break
+    end
+  end
+
+  is_prime
+
+end
